396c900ada01ec5253cf2cfa630a82568dd39fc2
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 12:50:58 +0530

    first commit

---
+++ WindowsFormsControlsDemo/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace WindowsFormsControlsDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelLocation = new System.Windows.Forms.Label();
+            this.labelGender = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.labelTechnology = new System.Windows.Forms.Label();
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.rbtnMale = new System.Windows.Forms.RadioButton();
+            this.rbtnFemale = new System.Windows.Forms.RadioButton();
+            this.txtBoxAddress = new System.Windows.Forms.TextBox();
+            this.cBoxLocation = new System.Windows.Forms.ComboBox();
+            this.lstBoxTechnology = new System.Windows.Forms.ListBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(273, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registration Form";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(83, 88);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(137, 21);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Enter your name ";
+            // 
+            // labelLocation
+            // 
+            this.labelLocation.AutoSize = true;
+            this.labelLocation.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLocation.Location = new System.Drawing.Point(83, 328);
+            this.labelLocation.Name = "labelLocation";
+            this.labelLocation.Size = new System.Drawing.Size(171, 21);
+            this.labelLocation.TabIndex = 2;
+            this.labelLocation.Text = "Choose your location";
+            // 
+            // labelGender
+            // 
+            this.labelGender.AutoSize = true;
+            this.labelGender.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGender.Location = new System.Drawing.Point(83, 153);
+            this.labelGender.Name = "labelGender";
+            this.labelGender.Size = new System.Drawing.Size(115, 21);
+            this.labelGender.TabIndex = 3;
+            this.labelGender.Text = "Select Gender";
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAddress.Location = new System.Drawing.Point(83, 209);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(151, 21);
+            this.labelAddress.TabIndex = 4;
+            this.labelAddress.Text = "Enter your address";
+            // 
+            // labelTechnology
+            // 
+            this.labelTechnology.AutoSize = true;
+            this.labelTechnology.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTechnology.Location = new System.Drawing.Point(83, 412);
+            this.labelTechnology.Name = "labelTechnology";
+            this.labelTechnology.Size = new System.Drawing.Size(194, 21);
+            this.labelTechnology.TabIndex = 5;
+            this.labelTechnology.Text = "Choose your technology";
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.Location = new System.Drawing.Point(342, 88);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(238, 20);
+            this.txtBoxName.TabIndex = 0;
+            // 
+            // rbtnMale
+            // 
+            this.rbtnMale.AutoSize = true;
+            this.rbtnMale.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnMale.Location = new System.Drawing.Point(342, 151);
+            this.rbtnMale.Name = "rbtnMale";
+            this.rbtnMale.Size = new System.Drawing.Size(58, 23);
+            this.rbtnMale.TabIndex = 1;
+            this.rbtnMale.TabStop = true;
+            this.rbtnMale.Text = "Male";
+            this.rbtnMale.UseVisualStyleBackColor = true;
+            // 
+            // rbtnFemale
+            // 
+            this.rbtnFemale.AutoSize = true;
+            this.rbtnFemale.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnFemale.Location = new System.Drawing.Point(428, 151);
+            this.rbtnFemale.Name = "rbtnFemale";
+            this.rbtnFemale.Size = new System.Drawing.Size(71, 23);
+            this.rbtnFemale.TabIndex = 2;
+            this.rbtnFemale.TabStop = true;
+            this.rbtnFemale.Text = "Female";
+            this.rbtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // txtBoxAddress
+            // 
+            this.txtBoxAddress.Location = new System.Drawing.Point(342, 209);
+            this.txtBoxAddress.Multiline = true;
+            this.txtBoxAddress.Name = "txtBoxAddress";
+            this.txtBoxAddress.Size = new System.Drawing.Size(238, 70);
+            this.txtBoxAddress.TabIndex = 3;
+            // 
+            // cBoxLocation
+            // 
+            this.cBoxLocation.FormattingEnabled = true;
+            this.cBoxLocation.Items.AddRange(new object[] {
+            "Chennai",
+            "Coimbatore",
+            "Madurai",
+            "Tirunelveli",
+            "Villupuram",
+            "Trichy",
+            "Tanjore",
+            "Vellore",
+            "Salem",
+            "Namakkal",
+            "Theni",
+            "Nagapattinam",
+            "Tanjore",
+            "Kanyakumari"});
+            this.cBoxLocation.Location = new System.Drawing.Point(342, 328);
+            this.cBoxLocation.Name = "cBoxLocation";
+            this.cBoxLocation.Size = new System.Drawing.Size(238, 21);
+            this.cBoxLocation.TabIndex = 4;
+            // 
+            // lstBoxTechnology
+            // 
+            this.lstBoxTechnology.FormattingEnabled = true;
+            this.lstBoxTechnology.Items.AddRange(new object[] {
+            "Java",
+            ".NET",
+            "Python",
+            "C",
+            "C++",
+            "SQL",
+            "Django",
+            "Visual Basic"});
+            this.lstBoxTechnology.Location = new System.Drawing.Point(348, 409);
+            this.lstBoxTechnology.Name = "lstBoxTechnology";
+            this.lstBoxTechnology.Size = new System.Drawing.Size(232, 69);
+            this.lstBoxTechnology.TabIndex = 5;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.Location = new System.Drawing.Point(348, 541);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(183, 46);
+            this.btnRegister.TabIndex = 6;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "label2";
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.Location = new System.Drawing.Point(680, 88);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(266, 390);
+            this.labelMessage.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.lstBoxTechnology);
+            this.Controls.Add(this.cBoxLocation);
+            this.Controls.Add(this.txtBoxAddress);
+            this.Controls.Add(this.rbtnFemale);
+            this.Controls.Add(this.rbtnMale);
+            this.Controls.Add(this.txtBoxName);
+            this.Controls.Add(this.labelTechnology);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.labelGender);
+            this.Controls.Add(this.labelLocation);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelLocation;
+        private System.Windows.Forms.Label labelGender;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Label labelTechnology;
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.RadioButton rbtnMale;
+        private System.Windows.Forms.RadioButton rbtnFemale;
+        private System.Windows.Forms.TextBox txtBoxAddress;
+        private System.Windows.Forms.ComboBox cBoxLocation;
+        private System.Windows.Forms.ListBox lstBoxTechnology;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelMessage;
+    }
+}
+
---
+++ WindowsFormsControlsDemo/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsControlsDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            var username = txtBoxName.Text;
+            var gender = string.Empty;
+            if (rbtnMale.Checked)
+                gender = rbtnMale.Text;
+            else
+                gender = rbtnFemale.Text;
+
+            var address = txtBoxAddress.Text;
+            var location = cBoxLocation.SelectedItem.ToString();
+            var technology = lstBoxTechnology.SelectedItem.ToString();
+
+            var message = $"Name={username} \n Gender={gender}\n Address={address}\n Location={location}\n Technology={technology}";
+
+            labelMessage.Text = message;
+
+            MessageBox.Show("Registration Successful");
+        }
+    }
+}
